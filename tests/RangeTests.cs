9b9d9d4fa44582cf3334674633c04e916d6b5938
Akachai <akacahi@wolfcomglobal.com>
2018-04-18 16:39:44 +0700

    first commit

---
+++ tests/RangeTests.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using api.Controllers;
+using Xunit;
+
+namespace tests
+{
+    public class RangeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var range = new Range {Count = 3};
+
+            var generated = range.Of(() => "");
+
+            Assert.Equal(3, generated.Count());
+        }
+
+    }
+    
+}
